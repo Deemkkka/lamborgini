a4b708a74ae95b86aa3f492ffd58fa536d0c48b5
Deem <Deemkkka@mail.ru>
2024-12-16 09:54:12 +0300

    with map

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ const swiper = new Swiper('.swiper', {
     },
 
   });
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
